1cc8670815a83c7052d6078dadf41ec8cf6f478a
My projects <valenciaamber52@gmail.com>
2025-06-30 01:07:05 +0200

    Add files via upload
    feat: create basic shopping cart with food input, price tracking, and total calculation
    
    - Allows user to input food items and prices in a loop
    - Stores items and prices in separate lists
    - Displays item list horizontally
    - Calculates and displays total price after user exits

---
+++ Shopping cart/list.py
@@ -0,0 +1,8 @@
+
+fruits = ["apple", "bananas", "cherry"]
+
+print(fruits[0])
+
+fruits[1]= "blueberry"
+
+print(fruits)
---
+++ Shopping cart/python.py
@@ -0,0 +1,16 @@
+#Operators
+
+#Addition(+)
+#Subtraction(-)
+#Multiplication
+#Division(/)
+#Modulus(%)
+#Exponent(**)
+
+x = 10
+y = 2
+
+x = 10
+x -= 2
+
+print(x)
